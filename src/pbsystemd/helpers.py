d5281557cc768d63b1f723e31159979d747d5051
Eric Pascual <eric@pobot.org>
2016-09-10 16:17:16 +0200

    extended to support any unit type

--- src/pbsystemd/helpers.py
+++ src/pbsystemd/helpers.py
@@ -13,12 +13,21 @@ ETC_SYSTEMD_SYSTEM = '/etc/systemd/system/'
 
 
 class SystemdSetupHelper(object):
-    def __init__(self, svc_name, setup_pkg_name, before_start=None, after_stop=None):
-        if not svc_name or not setup_pkg_name:
+    def __init__(self, unit_name, setup_pkg_name, before_start=None, after_stop=None):
+        if not unit_name or not setup_pkg_name:
             raise ValueError('missing mandatory parameter')
 
-        self._svc_name = svc_name
-        self._svc_file_name = svc_name + '.service'
+        self._unit_name = unit_name
+
+        # default to service if the unit name is not fully qualified
+        if unit_name.split('.')[-1] not in (
+                'service', 'socket', 'target', 'mount', 'device', 'automount',
+                'swap', 'path', 'timer', 'slice', 'scope'
+        ):
+            self._unit_file_name = unit_name + '.service'
+        else:
+            self._unit_file_name = unit_name
+
         self._setup_pkg_name = setup_pkg_name
 
         if before_start and not callable(before_start):
@@ -34,42 +43,42 @@ class SystemdSetupHelper(object):
         if not os.getuid() == 0:
             raise RuntimeError('must be root')
 
-    def install_service(self):
+    def install_unit(self):
         self._check_if_root()
 
         # copy the service descriptor file in the configuration directory
-        fn = pkg_resources.resource_filename(self._setup_pkg_name, 'pkg_data/%s') % self._svc_file_name
+        fn = pkg_resources.resource_filename(self._setup_pkg_name, 'pkg_data/%s') % self._unit_file_name
         shutil.copy(fn, ETC_SYSTEMD_SYSTEM)
         # make systemd be aware of changes
         subprocess.check_output(['systemctl', 'daemon-reload'])
         # enable the service at system start
-        subprocess.check_output(['systemctl', 'enable', self._svc_name])
+        subprocess.check_output(['systemctl', 'enable', self._unit_name])
         # (re)start it now
         if self._before_start:
             self._before_start()
-        subprocess.check_output(['systemctl', 'restart', self._svc_name])
+        subprocess.check_output(['systemctl', 'restart', self._unit_name])
 
         return True
 
-    def remove_service(self):
+    def remove_unit(self):
         self._check_if_root()
 
-        if not os.path.exists(os.path.join(ETC_SYSTEMD_SYSTEM, self._svc_file_name)):
+        if not os.path.exists(os.path.join(ETC_SYSTEMD_SYSTEM, self._unit_file_name)):
             return False
 
         # stop the service if currently running
         try:
-            subprocess.check_output(['systemctl', '-q', 'is-active', self._svc_name])
+            subprocess.check_output(['systemctl', '-q', 'is-active', self._unit_name])
         except subprocess.CalledProcessError:
             pass
         else:
-            subprocess.check_output(['systemctl', 'stop', self._svc_name])
+            subprocess.check_output(['systemctl', 'stop', self._unit_name])
             if self._after_stop:
                 self._after_stop()
 
         # remove it from system start
-        subprocess.check_output(['systemctl', 'disable', self._svc_name])
-        os.remove(os.path.join(ETC_SYSTEMD_SYSTEM, self._svc_file_name))
+        subprocess.check_output(['systemctl', 'disable', self._unit_name])
+        os.remove(os.path.join(ETC_SYSTEMD_SYSTEM, self._unit_file_name))
 
         # make systemd be aware of changes
         subprocess.check_output(['systemctl', 'daemon-reload'])
@@ -77,17 +86,21 @@ class SystemdSetupHelper(object):
         return True
 
 
-def install_service(svc_name, pkg, before_start=None):
+def install_unit(unit_name, pkg, before_start=None):
     try:
-        if not SystemdSetupHelper(svc_name, pkg, before_start=before_start).install_service():
+        if not SystemdSetupHelper(unit_name, pkg, before_start=before_start).install_unit():
             print("already installed")
     except RuntimeError as e:
         sys.exit("ERROR: %s" % e)
 
+install_service = install_unit      #: for backward compatibility
 
-def remove_service(svc_name, pkg, after_stop=None):
+
+def remove_unit(unit_name, pkg, after_stop=None):
     try:
-        if not SystemdSetupHelper(svc_name, pkg, after_stop=after_stop).remove_service():
+        if not SystemdSetupHelper(unit_name, pkg, after_stop=after_stop).remove_unit():
             print("not installed")
     except RuntimeError as e:
         sys.exit("ERROR: %s" % e)
+
+remove_service = remove_unit        #: for backward compatibility
